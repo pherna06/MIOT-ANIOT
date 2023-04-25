558519bb657d22cee94155563716838111aed48a
pherna06 <pablohernandezaguado@gmail.com>
2023-04-25 20:45:11 +0200

    - Removed Compose file. - Added idf-py.sh script to use idf.py from espressif/idf container. - Added idle project (to load on SoC to replace other projects).

---
+++ idf-py.sh
@@ -0,0 +1,22 @@
+CURRENTDIR=$(pwd)
+
+DOCKER_CMD="docker run --rm -it -v $CURRENTDIR:/project -w /project"
+DOCKER_IMG="espressif/idf"
+IDF_PY="idf.py"
+
+# Serial port
+while getopts ":p:" opt; do
+  case $opt in
+    p)
+        DOCKER_CMD="$DOCKER_CMD --device=$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Run idf.py command
+MAKE="$DOCKER_CMD $DOCKER_IMG $IDF_PY $@"
+echo $MAKE
+
+$MAKE
