f31774582683476ffd75364f5090c9a3d81fd744
pherna06 <pablohernandezaguado@gmail.com>
2023-06-25 16:27:52 +0200

    > Added multiversion support to Ex. 2 from Assignment 2

--- p_2/e_2/main/main.c
+++ p_2/e_2/main/main.c
@@ -1,98 +1,94 @@
-#include <stdio.h>
+// INCLUDES --------------------------------------------------------------------
 
-// Include for ADC functions and Hall sensor
-#include <driver/adc.h>
+/* Tasks */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-// include for ESP_LOGI and ESP_LOGW
+/* ADC */
+#include "driver/adc.h"
+
+/* Logging */
 #include <esp_log.h>
 
-// include for FreeRTOS functions
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+static const char *TAG = "app_main";
 
+// -----------------------------------------------------------------------------
 
+// DEFINES --------------------------------------------------------------------
 
-
-
-// Period of the main loop in miliseconds
+/* Period (ms) of the reading loop */
 #define PERIOD_MS 1000
 
-// Global variable to indicate that a new reading is available
-static int read = 0;
+// -----------------------------------------------------------------------------
 
+// TASKS ----------------------------------------------------------------------
 
-
-
-
-// Reading task
+/* Reading Task Parameters */
 struct reading_task_parameters
 {
     int period_ms;
-    int *ptr_to_hall;
+    int hall;
+    bool read;
 };
 
+/* Reading Task Function */
 void reading_task(void *pvParameter)
 {
-    static const char *TAG = "reading_task";
-    
     struct reading_task_parameters *params =
             (struct reading_task_parameters *)pvParameter;
 
-    ESP_LOGI(TAG, "Starting reading loop with %d ms period...", params->period_ms);
-    while (1)
-    {
-        // Sample Hall sensor
-        *(params->ptr_to_hall) = hall_sensor_read();
-        
-        // Notify main loop that a new reading is available
-        read = 1;
-
-        // Sleep for PERIOD_MS miliseconds
+    // > Hall Sensor Reading Loop
+    ESP_LOGI(TAG, "Starting reading loop with %d ms period...",
+        params->period_ms);
+    while (1) {
+        if (params->read) ESP_LOGW(TAG, "Overwriting unread reading!!!");
+        params->hall = hall_sensor_read();
+        params->read = true;
         vTaskDelay(params->period_ms / portTICK_PERIOD_MS);
     }
 }
 
+// -----------------------------------------------------------------------------
 
 
 
+// APP MAIN -------------------------------------------------------------------
 
-// Main function
 void app_main(void)
-{
-    static const char *TAG = "app_main";
+{    
+    esp_err_t err;
     
-    ESP_LOGI(TAG, "Start of app_main");
+    // > Log Hall Sensor GPIO use warning
     ESP_LOGW(TAG, "T\n> This app takes readings from the internal Hall sensor, "
                   "which uses channels 0 and 3 of ADC1 "
                   "(GPIO 36 and 39)"
                   "\n> Do not connect anything to these pings and "
                   "do not change their configuration!!!");
 
-    ESP_LOGI(TAG, "\n> Setting ADC1 precision:"
-                  "\n>   - 12 bit");
-    adc1_config_width(ADC_WIDTH_BIT_12);
+    
+    // > Enable ADC1 to use Hall Sensor (by configuring width)
+    if ( (err = adc1_config_width(ADC_WIDTH_BIT_12)) ) {
+        ESP_LOGE(TAG, "Could not set ADC1 width: %s", esp_err_to_name(err));
+        return;
+    }
 
-    int hall = 0; // Hall sensor reading
+    // > Create task parameters
+    struct reading_task_parameters params = {
+        .period_ms = PERIOD_MS ,
+        .hall      = 0         ,
+        .read      = false    };
 
-    // Create task parameters
-    struct reading_task_parameters params;
-    params.period_ms = PERIOD_MS;
-    params.ptr_to_hall = &hall;
-
-    // Create task
+    // > Create task
     xTaskCreate(&reading_task, "reading_task", 2048, &params, 5, NULL);
 
-    // Main loop
-    while (1)
-    {
-        // Check if a new reading is available
-        if (read)
-        {
-            ESP_LOGI(TAG, "Hall sensor: %d", hall);
-            read = 0;
+    // > Log when reading available loop
+    while (1) {
+        if (params.read) {
+            ESP_LOGI(TAG, "Hall sensor: %d", params.hall);
+            params.read = false;
         }
-
-        // Smallest delay possible to let idle task run (so watchdog is happy)
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(1);
     }
 }
+
+// -----------------------------------------------------------------------------
