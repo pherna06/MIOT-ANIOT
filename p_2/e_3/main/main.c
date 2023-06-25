8f79d549cd8da733ff29cbd5851f7da9e549ab83
pherna06 <pablohernandezaguado@gmail.com>
2023-06-25 16:38:55 +0200

    > Added multiversion support to Ex. 3 from Assignment 2

--- p_2/e_3/main/main.c
+++ p_2/e_3/main/main.c
@@ -1,65 +1,66 @@
-#include <stdio.h>
+// INCLUDES --------------------------------------------------------------------
 
-// Include for ADC functions and Hall sensor
-#include <driver/adc.h>
+/* Tasks & Queues */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 
-// include for ESP_LOGI and ESP_LOGW
+/* ADC */
+#include "driver/adc.h"
+
+/* Logging */
 #include <esp_log.h>
 
-// include for FreeRTOS functions
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
+static const char *TAG = "app_main";
 
+// -----------------------------------------------------------------------------
 
+// DEFINES --------------------------------------------------------------------
 
-
-
-// Period of the main loop in miliseconds
+/* Period (ms) of the reading loop */
 #define PERIOD_MS 1000
 
+/* Size of readings queue */
+#define QUEUE_SIZE 1
 
+// -----------------------------------------------------------------------------
 
+// TASKS ----------------------------------------------------------------------
 
-
-// Reading task
+/* Reading Task Parameters */
 struct reading_task_parameters
 {
     int period_ms;
     QueueHandle_t hall_queue;
 };
 
+/* Reading Task Function */
 void reading_task(void *pvParameter)
 {
-    static const char *TAG = "reading_task";
+    BaseType_t err;
     
     struct reading_task_parameters *params =
             (struct reading_task_parameters *)pvParameter;
 
+    // > Hall Sensor Reading Loop
     ESP_LOGI(TAG, "Starting reading loop with %d ms period...", params->period_ms);
-    while (1)
-    {
-        // Sample Hall sensor
+    while (1) {
         int hall = hall_sensor_read();
-
-        // Send to queue (unchecked)
-        if (params->hall_queue != NULL)
-            xQueueSend(params->hall_queue, &hall, 0);
-
-        // Sleep for PERIOD_MS miliseconds
+        if ( (err = xQueueSend(params->hall_queue, &hall, 0)) != pdTRUE )
+            ESP_LOGW(TAG, "Error sending hall reading to queue: %d", err);
         vTaskDelay(params->period_ms / portTICK_PERIOD_MS);
     }
 }
 
+// -----------------------------------------------------------------------------
 
+// APP MAIN -------------------------------------------------------------------
 
-
-
-// Main function
 void app_main(void)
 {
-    static const char *TAG = "app_main";
-    
+    esp_err_t err;
+
+    // > Log Hall Sensor GPIO use warning
     ESP_LOGI(TAG, "Start of app_main");
     ESP_LOGW(TAG, "T\n> This app takes readings from the internal Hall sensor, "
                   "which uses channels 0 and 3 of ADC1 "
@@ -67,29 +68,38 @@ void app_main(void)
                   "\n> Do not connect anything to these pings and "
                   "do not change their configuration!!!");
 
-    ESP_LOGI(TAG, "\n> Setting ADC1 precision:"
-                  "\n>   - 12 bit");
-    adc1_config_width(ADC_WIDTH_BIT_12);
+    // > Enable ADC1 to use Hall Sensor (by configuring width)
+    if ( (err = adc1_config_width(ADC_WIDTH_BIT_12)) ) {
+        ESP_LOGE(TAG, "Could not set ADC1 width: %s", esp_err_to_name(err));
+        return;
+    }
 
-    // Hall readings queue
-    QueueHandle_t hall_queue = xQueueCreate(1, sizeof(int));
+    // > Create Queue for Hall Sensor readings
+    QueueHandle_t hall_queue = xQueueCreate(QUEUE_SIZE, sizeof(int));
 
-    // Create task parameters
-    struct reading_task_parameters params;
-    params.period_ms = PERIOD_MS;
-    params.hall_queue = hall_queue;
+    // > Create task parameters
+    struct reading_task_parameters params = {
+        .period_ms  = PERIOD_MS   ,
+        .hall_queue = hall_queue };
 
-    // Create task
+    // > Create task
     xTaskCreate(&reading_task, "reading_task", 2048, &params, 5, NULL);
 
-    // Main loop
-    int hall;
-    int queue_wait = 10 / portTICK_PERIOD_MS;
-    while (1)
-    {
-        // Check if a new reading is available in the queue
-        if (hall_queue != NULL
-                && xQueueReceive(hall_queue, &hall, queue_wait) == pdTRUE)
-            ESP_LOGI(TAG, "Hall sensor: %d", hall);
+    // > Receive readings from queue and log them (loop)
+    while (1) {
+        BaseType_t rtos;
+        int hall;
+
+        // > Receive reading from queue
+        rtos = xQueueReceive(hall_queue, &hall, portMAX_DELAY);
+        if (rtos != pdTRUE) {
+            ESP_LOGE(TAG, "Error receiving reading from queue: %d", rtos);
+            continue;
+        }
+
+        // > Log reading
+        ESP_LOGI(TAG, "Hall sensor: %d", hall);
     }
 }
+
+// -----------------------------------------------------------------------------
