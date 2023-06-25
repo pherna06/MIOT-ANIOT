bed95056f5fcd0beeebaa10f670dc9bd0f8236d2
pherna06 <pablohernandezaguado@gmail.com>
2023-06-25 14:46:00 +0200

    > Updated IDF Docker Script to allow for custom `espressif/idf` image tags. This way, we can use different IDF versions. The only supported tags are those corresponding to the next IDF releases: `release-v4.2`, `release-v4.3`, `release-v4.4`, `release-v5.0`, `release-v5.1`.

--- idf-py.sh
+++ idf-py.sh
@@ -1,22 +1,48 @@
 CURRENTDIR=$(pwd)
 
-DOCKER_CMD="docker run --rm -it -v $CURRENTDIR:/project -w /project"
-DOCKER_IMG="espressif/idf:release-v4.4"
-IDF_PY="idf.py"
+# Docker Command
+DOCKER_CMD="docker run"
+DOCKER_CMD_ARGS="--rm -it -v $CURRENTDIR:/project -w /project"
+DOCKER_BASE_IMG="espressif/idf"
 
-# Serial port
-while getopts ":p:" opt; do
-  case $opt in
-    p)
-        DOCKER_CMD="$DOCKER_CMD --device=$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
+# idf.py Command
+IDF_PY="idf.py"
+IDF_PY_ARGS=""
+# process arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -t|--tag)
+        DOCKER_IMG="$DOCKER_BASE_IMG:$2"
+
+        if   [[ $2 == "release-v4.*" ]]; then
+            DOCKER_CMD_ARGS="$DOCKER_CMD_ARGS -e CMAKE_VER=3.5"
+        elif [[ $2 == "release-v5.*" ]]; then
+            DOCKER_CMD_ARGS="$DOCKER_CMD_ARGS -e CMAKE_VER=3.16"
+        else
+            DOCKER_CMD_ARGS="$DOCKER_CMD_ARGS -e CMAKE_VER=3.5"
+        fi
+        shift # past argument
+        shift # past value
+        ;;
+
+        -p|--port) # add device to docker run
+        DOCKER_CMD_ARGS="$DOCKER_CMD_ARGS --device=$2"
+        shift # past argument
+        shift # past value
+        ;;
+
+        *)    # add to idf.py args
+        IDF_PY_ARGS="$IDF_PY_ARGS $1"
+        shift # past argument
+        ;;
+    esac
 done
 
 # Run idf.py command
-MAKE="$DOCKER_CMD $DOCKER_IMG $IDF_PY $@"
-echo $MAKE
+MAKE="$DOCKER_CMD $DOCKER_CMD_ARGS $DOCKER_IMG $IDF_PY $IDF_PY_ARGS"
+echo "> Running: $MAKE"
 
 $MAKE
